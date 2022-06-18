42ffc3c6f5d8ade7a0c70e06f938321429d627a9
Takatoshi Kondo <redboltz@gmail.com>
2022-06-19 00:33:55 +0900

    Added bench.

--- bench.sh
+++ bench.sh
@@ -7,4 +7,4 @@ then
 fi
 echo "tag $TAG is used"
 docker pull redboltz/mqtt_cpp:$TAG
-docker run -v $PWD/conf:/var/mqtt_cpp_build/example/conf -it redboltz/mqtt_cpp:$TAG ../client_cli $@
+docker run -v $PWD/conf:/var/mqtt_cpp_build/example/conf -it redboltz/mqtt_cpp:$TAG ../bench $@
